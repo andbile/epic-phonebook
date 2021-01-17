17197f2c7bb81e1d85abd8369e671012b3de1ffe
Andrii Biletskyi <andbile@gmail.com>
2021-01-17 19:22:44 +0200

    Added sql test data

---
+++ ressource/bd/db.sql
@@ -0,0 +1,61 @@
+-- database
+CREATE DATABASE uman
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE uman IS 'Корпоративний сайт ТЦ «Епіцентр К», м. Умань';
+
+-- departments
+CREATE TABLE department
+(
+    department_id int PRIMARY KEY,
+    department_name varchar(256) NOT NULL
+
+);
+COMMENT ON TABLE department IS 'Коди та назви департаментів';
+
+
+-- employees
+CREATE TABLE employee
+(
+    employee_id int PRIMARY KEY,
+    department_id int NOT NULL,
+    first_name varchar (20) NOT NULL,
+    last_name varchar (30) NOT NULL,
+    patronymic_name varchar(30) NOT NULL,
+    position varchar,
+    isChief boolean,
+    tel_number_dect varchar,
+    tel_number_landline varchar,
+    email varchar ARRAY
+);
+COMMENT ON TABLE employee IS 'Службовці';
+
+
+
+-- Смена типа данных
+-- Необязательное предложение USING определяет, как новое значение столбца будет получено из старого; если оно отсутствует,
+-- выполняется приведение типа по умолчанию, как обычное присваивание значения старого типа новому.
+-- Предложение USING становится обязательным, если неявное приведение или присваивание с приведением старого типа к новому не определено.
+ALTER TABLE employee
+    ALTER COLUMN tel_number_dect SET DATA TYPE varchar USING tel_number_dect::varchar;
+
+ALTER TABLE employee
+    ALTER COLUMN tel_number_landline SET DATA TYPE varchar USING tel_number_landline::varchar;
+
+
+-- добавление колонки
+ALTER TABLE employee
+    ADD COLUMN IF NOT EXISTS email varchar ARRAY[];
+
+ALTER TABLE employee
+    ALTER COLUMN email SET DATA TYPE varchar ARRAY USING email::varchar[];
+
+ALTER TABLE employee
+    ADD COLUMN IF NOT EXISTS position varchar;
+
+
+-- очистка/удаление таблицы
+TRUNCATE employee RESTART IDENTITY;
+
+DROP TABLE IF EXISTS employee;
