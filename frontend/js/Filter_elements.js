31d5b2314276989fc4d51ccccd223a51f5aee438
Andrii Biletskyi <andbile@gmail.com>
2021-02-18 19:41:19 +0200

    Added filter for phonebook

---
+++ frontend/js/Filter_elements.js
@@ -0,0 +1,78 @@
+/**
+ * Фільтрація елементів
+ * Фільтрація виконується натисканням на кнопки
+ * ------------------------------------------------------------- */
+
+export default class FilterElements {
+
+    /**
+     * @type {object} options - налаштування фільтру
+     * @property {string} options.parentElement - css селектор батьківського елемента
+     * @property {string} options.btns - css селектор
+     * @property {string} options.elements - css селектор
+     * @property {string} options.dataFilter - data-атрибут для пошуку і фільтрації елементів
+     * @property {string} options.resetFilter - data-атрибут для скидання фільтру
+     */
+    constructor(options) {
+        this.options = options
+
+        this.parentElement = document.querySelector(options.parentElement)
+        // Елементи для фільтрування
+        this.filterElements = this.parentElement.querySelectorAll(this.options.elements.itemSelector)
+
+        // Встановлення обробників на кнопки
+        this.clickHandler = this.clickHandler.bind(this)
+        this.setHandlers()
+
+    }
+
+
+    /**
+     * Перевіряємо наявність батьківського елемента для фільтрації
+     * @param {string} selector - css селектор
+     * @returns {boolean}
+     */
+    static isParentElementFound (selector){
+        try{
+            return !!document.querySelector(selector);
+        }catch{
+            return false;
+        }
+    }
+
+    // Встановлення обробників на кнопки
+    setHandlers(){
+        const btns = this.parentElement.querySelectorAll(this.options.btns.itemSelector)
+
+        btns.forEach(item => {
+            item.addEventListener('click', this.clickHandler)
+        })
+    }
+
+
+    clickHandler(evt){
+        const target = evt.target
+
+        /** @type {string} currentFilter - фільтр */
+        const currentFilter = target.dataset[this.options.dataFilter]
+
+        this.filter(currentFilter)
+    }
+
+    /** @param {string} currentFilter - фільтр з data-атрибуту кнопки */
+    filter(currentFilter){
+
+        // скидаємо фільтри і відображаємо всю інформацію
+        if(currentFilter === this.options.resetFilter) {
+            this.filterElements.forEach(item =>{
+                item.hidden = false
+            })
+            return
+        }
+
+        // фільтруємо, приховуючи все, що не дорівнює currentFilter
+        this.filterElements.forEach( item =>{
+            item.hidden = item.dataset.filter !== currentFilter
+        })
+    }
+}
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,33 @@
-console.log('ok');
-console.log('ok2');
-console.log('ok11');
-console.log('ok12');
+import FilterElements from './Filter_elements.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    /**
+     * Фільтрація "Довідника телефонних номерів та електронної пошти"
+     * Фільтруємо по торгових/не торгових/всіх
+     * ------------------------------------------------------------- */
+    const isParentElementFound = FilterElements.isParentElementFound('#js-phone-book')
+    if(isParentElementFound){
+        new FilterElements({
+            parentElement: '#js-phone-book',
+            btns:{
+                itemSelector: '.btn-item',
+            },
+            elements:{
+                itemSelector: '.filter-item'
+            },
+            dataFilter: 'filter',
+            resetFilter: 'all'
+        })
+    }
+
+
+
+
+
+
+
+});
+
+
+
