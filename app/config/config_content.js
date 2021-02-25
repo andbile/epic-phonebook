928c5f7944987903e869963358a5e80868d14673
Andrii Biletskyi <andbile@gmail.com>
2021-02-25 20:07:59 +0200

    Added title tag content

---
+++ app/config/config_content.js
@@ -0,0 +1,12 @@
+/**
+ *  General site content for handlebars templates
+ *  @type {Object}
+ *  @type {{headTitle: string}} - <title> tag content
+ *
+ *
+ */
+
+
+module.exports = {
+    headTitle : 'Корпоративний сайт ТЦ «Епіцентр К», м. Умань'
+}
