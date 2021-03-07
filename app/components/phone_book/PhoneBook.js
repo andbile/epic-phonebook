bb932c10cd784edbcd1f8446eb73265d2352d83f
Andrii Biletskyi <andbile@gmail.com>
2021-03-07 15:45:00 +0200

    Added comments and error handling PhoneBook.js

--- app/components/phone_book/PhoneBook.js
+++ app/components/phone_book/PhoneBook.js
@@ -1,8 +1,9 @@
 const {pool} = require('../../bd/connect') // connect to BD
 
-//TODO коментарий и рефракторинг
 //TODO поменять гит на SSH
-// TODO обработка ощибок
+
+// TODO Пошук по прізвищам
+// TODO Мобільні номера
 
 /**
  * telephone and e-mail directory
@@ -15,70 +16,92 @@ class PhoneBook{
         this.departments = {} // list of departments from BD
         this.employees = {} // list of employees from BD
         this.phoneBook = {} // the resulting object for the template engine
+        this.error = '' // error massage
 
         this.render()
     }
 
-
+    // get departments from DB
     async getDepartments(){
         const row = await pool.query('SELECT * FROM department')
         return row.rows
     }
 
+    // get employees from DB
     async getEmployees(){
         const row = await pool.query('SELECT * FROM employee')
         return row.rows
     }
 
-    async getAllDataFromBD (){
-        try{
-            const [departments, employees] = await Promise.all([this.getDepartments(), this.getEmployees()]);
-            return {departments, employees}
-        }catch (err){
-            // TODO обрабатывать как-то ошибки
-            console.log(err)
-        }
+    // get departments and employees from DB
+    async getPhoneBookData (){
+        const [departments, employees] = await Promise.all([this.getDepartments(), this.getEmployees()]);
+        return {departments, employees}
     }
 
 
-    // get resulting object
+    /**
+     * Create resulting object for the template engine
+     * Create object where:
+     *  key - department_id, value - object with properties:
+     *      department_name {string},
+     *      department_isSeller {boolean} - for sorting on the client
+     *      employees {array} - array of employees
+     */
     getResultPhoneBookObj(){
         this.departments.forEach( item => {
             this.phoneBook[item['department_id']] = this.setDepartments(item);
 
             this.setEmployees(item)
-
         })
     }
 
-    setDepartments(obj){
+    /**
+     * Create object
+     * @param {object} department - item of the departments list from BD
+     * @returns {object} - {department_name: string, department_isSeller: boolean, employees: array}
+     */
+    setDepartments(department){
         return {
-            department_name: obj.department_name,
-            department_isSeller: obj.is_seller,
+            department_name: department.department_name,
+            department_isSeller: department.is_seller,
             employees: []
         }
     }
 
-    setEmployees(departmentItem){
-        const id = departmentItem['department_id']
+    /**
+     * Fill array by the list of employees of the relevant department
+     * Filter array using id
+     * @param department {object} - item of the departments' list
+     */
+    setEmployees(department){
+        const id = department['department_id']
         this.phoneBook[id].employees = this.employees.filter( item => item['department_id'] === id)
     }
 
 
     async render(){
 
-        await this.getAllDataFromBD().then( (object) => {
+        await this.getPhoneBookData().then( (object) => {
             const {departments, employees} = object
             this.departments = departments
             this.employees = employees
+        }).catch( err => {
+
+            this.error = `Сталася помилка ${err.code}, спробуйте пізніше`
+            this.res.render('errors/error', {
+                error: this.error
+            })
         })
 
-        // get resulting object
-        await this.getResultPhoneBookObj();
+        if(!this.error){
+            // get resulting object
+            await this.getResultPhoneBookObj();
 
-        this.res.render('phone_book/phone_book', {
-            phoneBook:this.phoneBook
-        })
+            this.res.render('phone_book/phone_book', {
+                phoneBook:this.phoneBook
+            })
+        }
     }
 }
 
