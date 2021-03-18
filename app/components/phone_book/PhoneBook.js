36c40e8d65f558a6e345d9ab6852117b2ba06afe
Andrii Biletskyi <andbile@gmail.com>
2021-03-18 02:01:59 +0200

    Added general department contacts

--- app/components/phone_book/PhoneBook.js
+++ app/components/phone_book/PhoneBook.js
@@ -2,6 +2,19 @@ const {pool} = require('../../bd/connect') // connect to BD
 
 // TODO Пошук по прізвищам
 // TODO Мобільні номера
+// TODO При додаванні контакту перевіряти наявність (чи вибирати із списку) відділ
+// TODO Придумати щось зі здвоєними відділами. Асистентів відділів копіювати у декілька відділів.
+// TODO Об'явити в групі, якщо э корпоративні номера можна забити лише їх. Змінити відділ якщо числиться
+//  в одному відділі а по факту працює в іншому відділі
+// TODO Якщо асистент двох відділів, додавати у оба, продумати цей нюанс в адмінці
+// TODO Перевірте всі дані особисто, якщо якоюсь поштою не користуєтесь, подзвоніть мені, видалимо її
+// TODO Зробити перевірку, щоб дві пошти в одну строку не засунули, приклад ['um.info50@epicentrk.com, um.info100@epicentrk.com']
+// TODO Зробити окрему строку: загальні номере відділу (DECT, стац, телефони кас), загальна пошта, якщо є
+// TODO Якщо багато поштових ящиків (фінік), можна залишити один основний
+// TODO При виборі якогось підрозділу, інші ховати. Кнопка показати всіх.
+// TODO Проверяйте может кого-то забыл или не в тот отдел вставил
+// TODO Зробити опис (пояснення) в адмінці: якщо є загальні номери, то всы ПІБ їх не прописувати
+// TODO Якщо є пропозиції по довіднику - пишіть на пошту
 
 /**
  * telephone and e-mail directory
@@ -21,13 +34,13 @@ class PhoneBook{
 
     // get departments from DB
     async getDepartments(){
-        const row = await pool.query('SELECT * FROM department')
+        const row = await pool.query('SELECT * FROM department_contacts RIGHT JOIN department on department.department_code = department_contacts.department_code ORDER BY department.department_code')
         return row.rows
     }
 
     // get employees from DB
     async getEmployees(){
-        const row = await pool.query('SELECT * FROM employee')
+        const row = await pool.query('SELECT * FROM employee ORDER BY last_name')
         return row.rows
     }
 
@@ -48,7 +61,7 @@ class PhoneBook{
      */
     getResultPhoneBookObj(){
         this.departments.forEach( item => {
-            this.phoneBook[item['department_id']] = this.setDepartments(item);
+            this.phoneBook[item['department_code']] = this.setDepartments(item);
 
             this.setEmployees(item)
         })
@@ -58,11 +71,26 @@ class PhoneBook{
      * Create object
      * @param {object} department - item of the departments list from BD
      * @returns {object} - {department_name: string, department_isSeller: boolean, employees: array}
+     *
+     *
      */
     setDepartments(department){
         return {
             department_name: department.department_name,
+            department_code: department.department_code,
             department_isSeller: department.is_seller,
+            // !!! TODO описать добавление новых контактов is_department_contacts сли есть хоть один контакт добовляем
+            //      * в шаблонизаторе - пеерделать!!!
+            is_department_contacts: ( () => {
+                if (department.tel_number_dect || department.tel_number_landline ||
+                    department.tel_number_mobile || department.email){
+                    return true
+                }else return false
+            })(),
+            tel_number_dect: department.tel_number_dect,
+            tel_number_landline: department.tel_number_landline,
+            tel_number_mobile: department.tel_number_mobile,
+            email:department.email,
             employees: []
         }
     }
@@ -73,8 +101,8 @@ class PhoneBook{
      * @param department {object} - item of the departments' list
      */
     setEmployees(department){
-        const id = department['department_id']
-        this.phoneBook[id].employees = this.employees.filter( item => item['department_id'] === id)
+        const departmentCode = department['department_code']
+        this.phoneBook[departmentCode].employees = this.employees.filter( item => item['department_code'] === departmentCode)
     }
 
 
--- ressource/bd/db.sql
+++ ressource/bd/db.sql
@@ -2,34 +2,85 @@
 CREATE DATABASE uman
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
-
 COMMENT ON DATABASE uman IS 'Корпоративний сайт ТЦ «Епіцентр К», м. Умань';
+
 
 -- departments
 CREATE TABLE department
 (
-    department_id int PRIMARY KEY,
-    department_name varchar(256) NOT NULL
-
+    department_code VARCHAR(32) PRIMARY KEY NOT NULL,
+    department_name VARCHAR(256) UNIQUE NOT NULL,
+    is_seller       BOOLEAN NOT NULL
 );
-COMMENT ON TABLE department IS 'Коди та назви департаментів';
+COMMENT ON TABLE department IS 'Коди та назви відділів';
+
+
+
+
+-- departments_contacts
+CREATE TABLE departments_contacts
+(
+    department_code     VARCHAR(32) PRIMARY KEY REFERENCES department(department_code) UNIQUE NOT NULL,
+    tel_number_dect     VARCHAR(32) DEFAULT NULL,
+    tel_number_landline VARCHAR(32) DEFAULT NULL,
+    tel_number_mobile   VARCHAR(32) DEFAULT NULL,
+    email               VARCHAR(32) DEFAULT NULL
+);
+COMMENT ON TABLE department_contacts IS 'Загальні телефони на електронні адреси відділів';
+
+
+
+-- PRIMARY KEY
+
+ALTER TABLE department_contacts
+    DROP CONSTRAINT department_code_fkey;
+
+
+ALTER TABLE employee
+    ADD CONSTRAINT departments_code_employee_fkey FOREIGN KEY(department_code) REFERENCES department(department_code);
+
+
+
+
+ALTER TABLE department_contacts
+    RENAME COLUMN fk_department_code TO department_code;
+
+
+ALTER TABLE department_contacts
+    ADD CONSTRAINT department_code_contacts_pkey PRIMARY KEY (department_code);
+
+
+
 
 
 -- employees
 CREATE TABLE employee
 (
-    employee_id int PRIMARY KEY,
-    department_id int NOT NULL,
-    first_name varchar (20) NOT NULL,
-    last_name varchar (30) NOT NULL,
-    patronymic_name varchar(30) NOT NULL,
-    position varchar,
-    isChief boolean,
-    tel_number_dect varchar,
-    tel_number_landline varchar,
-    email varchar ARRAY
+    employee_id         SERIAL PRIMARY KEY,
+    department_code     VARCHAR(32) NOT NULL,
+    first_name          VARCHAR(32) NOT NULL,
+    last_name           VARCHAR(64) NOT NULL,
+    patronymic_name     VARCHAR(64) NOT NULL,
+    position            VARCHAR,
+    tel_number_dect     VARCHAR(32) DEFAULT NULL,
+    tel_number_landline VARCHAR(32) DEFAULT NULL,
+    tel_number_mobile   VARCHAR ARRAY DEFAULT NULL,
+    email               VARCHAR ARRAY DEFAULT NULL
 );
 COMMENT ON TABLE employee IS 'Службовці';
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,6 +94,52 @@ ALTER TABLE employee
 ALTER TABLE employee
     ALTER COLUMN tel_number_landline SET DATA TYPE varchar USING tel_number_landline::varchar;
 
+ALTER TABLE employee
+    ALTER COLUMN department_id SET DATA TYPE varchar USING department_id::varchar;
+
+ALTER TABLE employee
+    ALTER COLUMN department_id SET DATA TYPE varchar USING department_id::varchar;
+
+ALTER TABLE employee
+    alter column tel_number_landline drop default,
+    alter column tel_number_landline type VARCHAR[] using array[tel_number_landline],
+    alter column tel_number_landline set default null;
+
+SELECT tel_number_landline FROM employee;
+
+UPDATE employee
+SET tel_number_landline = NULL
+WHERE tel_number_landline[1] IS NULL;
+
+
+-- добавление колонки c внешним ключом
+-- Сделать нуль
+ALTER TABLE department_contacts
+ADD COLUMN fk_department_code VARCHAR(32);
+
+
+ALTER TABLE department_contacts
+ADD CONSTRAINT fk_department_code FOREIGN KEY(fk_department_code) REFERENCES department(department_code);
+
+
+
+
+-- PRIMARY KEY
+ALTER TABLE department
+    DROP CONSTRAINT department_pkey;
+
+
+ALTER TABLE department
+    ADD CONSTRAINT department_code_pkey PRIMARY KEY (department_code);
+
+
+-- добовление автоинкримента к уже существующей таблице, надо было использовать псеводотип serial
+CREATE SEQUENCE employee_id_seq START 8;
+ALTER TABLE employee ALTER employee_id SET DEFAULT NEXTVAL('employee_id_seq');
+
+
+
+
 
 -- добавление колонки
 ALTER TABLE department
@@ -51,6 +148,9 @@ Alter table department alter column isSeller set not null;
 
 ALTER TABLE employee
     ADD COLUMN IF NOT EXISTS email varchar ARRAY[];
+
+ALTER TABLE department
+    DROP COLUMN department_id;
 
 ALTER TABLE employee
     ALTER COLUMN email SET DATA TYPE varchar ARRAY USING email::varchar[];
@@ -64,3 +164,5 @@ ALTER TABLE department RENAME COLUMN isseller TO is_seller;
 -- очистка/удаление таблицы
 TRUNCATE employee RESTART IDENTITY;
 DROP TABLE IF EXISTS employee;
+
+
