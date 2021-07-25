bc2e4c32fcb4930c821253d2f31f3b5b6a247083
Andrii Biletskyi <andbile@gmail.com>
2021-07-25 16:02:29 +0300

    Deleted unused import

--- client/src/pages/departments/phonebook/EmployeesPhoneBookModalAdmin.js
+++ client/src/pages/departments/phonebook/EmployeesPhoneBookModalAdmin.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {FormControl, InputGroup, Modal} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import {Context} from "../../../index";
