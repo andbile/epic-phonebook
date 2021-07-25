e618cd948fec289c66d397e3ab4f07b1d201fb9a
Andrii Biletskyi <andbile@gmail.com>
2021-07-25 16:01:22 +0300

    Get a name to the functions

--- client/src/components/propTypeValidators/isDepartmentId.js
+++ client/src/components/propTypeValidators/isDepartmentId.js
@@ -1,8 +1,10 @@
-export default props => {
+const isDepartmentId = props => {
     const {isAdminPanel, departmentId} = props
     if (isAdminPanel) {
         if (typeof departmentId != 'number') {
-            return new Error( `The prop "departmentId" required number, but its value '${typeof departmentId}'` )
+            return new Error(`The prop "departmentId" required number, but its value '${typeof departmentId}'`)
         }
     }
 }
+
+export default isDepartmentId
--- client/src/components/propTypeValidators/isPhoneBookBtnCallbacks.js
+++ client/src/components/propTypeValidators/isPhoneBookBtnCallbacks.js
@@ -1,16 +1,18 @@
 // propTypes validators event handlers - update/delete phone book entry
-export default props => {
+const isPhoneBookBtnCallbacks = props => {
     const {isAdminPanel, phoneBookBtnCallbacks} = props
     if (isAdminPanel) {
         if (typeof phoneBookBtnCallbacks != 'object')
-            return new Error( `The prop "phoneBookBtnCallbacks" required object, but its value '${typeof phoneBookBtnCallbacks}'` )
+            return new Error(`The prop "phoneBookBtnCallbacks" required object, but its value '${typeof phoneBookBtnCallbacks}'`)
 
         if (typeof phoneBookBtnCallbacks.updatePhoneBookEntry != 'function') {
-            return new Error( `The prop "phoneBookBtnCallbacks.updatePhoneBookEntry" required function, but its value '${typeof phoneBookBtnCallbacks.updatePhoneBookEntry}'` )
+            return new Error(`The prop "phoneBookBtnCallbacks.updatePhoneBookEntry" required function, but its value '${typeof phoneBookBtnCallbacks.updatePhoneBookEntry}'`)
         }
 
         if (typeof phoneBookBtnCallbacks.deletePhoneBookEntry != 'function') {
-            return new Error( `The prop "phoneBookBtnCallbacks.deletePhoneBookEntry" required function, but its value '${typeof phoneBookBtnCallbacks.deletePhoneBookEntry}'` )
+            return new Error(`The prop "phoneBookBtnCallbacks.deletePhoneBookEntry" required function, but its value '${typeof phoneBookBtnCallbacks.deletePhoneBookEntry}'`)
         }
     }
 }
+
+export default isPhoneBookBtnCallbacks
