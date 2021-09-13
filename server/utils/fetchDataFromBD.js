658094f3950bbf06111516cb469f46a4d772e3c4
Andrii Biletskyi <andbile@gmail.com>
2021-09-13 20:02:10 +0300

    Fixed handler error

--- server/utils/fetchDataFromBD.js
+++ server/utils/fetchDataFromBD.js
@@ -14,11 +14,11 @@ module.exports = async function (callback, req, res, next) {
    return callback()
         .catch(err => {
                 if (err instanceof Sequelize.ValidationError) {
-                    return next(ApiError.badRequest(`${err.original}  ${err.parent.detail}`))
+                    return next(ApiError.badRequest(`${err}`))
 
                 } else if (err instanceof Sequelize.DatabaseError) {
-                    console.log(`${err.original.code} ${err}`)
-                    return next(ApiError.internal(`Помилка в запиті до бази даних ${err.original.code}`))
+                    console.log(`${err}`)
+                    return next(ApiError.internal(`Помилка в запиті до бази даних ${err?.original?.code}`))
 
                 } else {
                     console.log(err)
