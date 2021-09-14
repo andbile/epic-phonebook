fa9967425457ce68530daa585aca0a68cbc805e1
Andrii Biletskyi <andbile@gmail.com>
2021-09-14 14:14:22 +0300

    Added comments

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 
-// для обычных запросов которе не требует авторизации
+// unauthorized users
 const $host = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 })
 
-// требующих авторизацию
-// каждому запросу автоматически будет подставляться header.authorization
+
+// authorized users
 const $authHost = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 })
 
+// adding a token to requests requiring authorization
 const authInterceptor = config => {
     config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
     return config
 }
-
-// добавляем интерцептор на запрос
-// будет отрабатывать перед каждым запросом и будет подставлять токен
 $authHost.interceptors.request.use(authInterceptor)
+
 
 export {
     $host,
