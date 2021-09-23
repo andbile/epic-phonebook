a14dc01dc7ecbaf6b3c05236ef6fca67ee03262d
Andrii Biletskyi <andbile@gmail.com>
2021-09-23 22:00:46 +0300

    Fix env config

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 const express = require('express')
 const sequelize = require('./db')
 //const models = require('./models/models') // модели БД
 const cors = require('cors')
-const path = require('path')
 const router = require('./routes/index') // основной маршрут
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-
 const PORT = process.env.PORT || 5000
+
 const app = express()
 app.use(cors()) // что бы могли отправлять запросы с браузера
 //app.use(express.urlencoded());
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
-app.use('/', router) // подключаем роутер
-
+app.use('/api', router) // подключаем роутер
 
 
 app.use(errorHandler) // Обработка ошибок
